01379a466db38c02d31e1d9a4382d69926106c38
Lawson Kurtz <lawson.kurtz@gmail.com>
2018-06-23 14:22:32 -0600

    Finish argument setup

--- pycrypt.py
+++ pycrypt.py
@@ -1,15 +1,18 @@
 import click
 import secrets
 
+@click.argument('path_or_message', nargs=-1)
 @click.command()
 @click.option('-k', '--key', default="random", prompt="Enter a key or press Enter to use a random key", hide_input=True, help="A key for encryption/decryption")
 
-def main(key):
+def main(path_or_message, key):
     click.echo('Welcome to PyCrypt!')
+    path_or_message = ' '.join(path_or_message)
     if key == "random":
-        key = secrets.token_bytes(32)
+        key = secrets.token_hex(32)
 
-    click.echo("Key is: " + str(key))
+    click.echo('Encrypting: ' + path_or_message)
+    click.echo("Using key: " + str(key))
 
 
 if __name__ == '__main__':
